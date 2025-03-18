719115f0b347fd0a4b9d009bf9544699d03938a0
shawon243196 <sadmanshawon542@gmail.com>
2025-03-18 17:18:00 +0600

    second commit

--- code.cpp
+++ code.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include<iomanip>
 #include<string.h>
+#include <climits>
+#include <vector>
+#include<bits/stdc++.h>
 using namespace std;
 
 #define fasterIO ios::sync_with_stdio(0); cin.tie(0);
@@ -13,10 +16,39 @@ int main()
     freopen("input.txt","r+",stdin);
     freopen("output.o","w+",stdout);
     #endif
-int x,y;
-cin>>x>>y;
-int a,b;
-a=y/2;
-b=x-a;cout<<b;
+    int t;
+    cin>>t;
+    while(t--){
+int n;
+cin>>n;
+vector<int>v(n);
+int count=0;
+for(int i=0;i<n;i++)
+{
+    cin>>v[i];
+}
+for (int i = 0; i < n; i++) 
+   {
+    for (int j = i; j < n; j++)
+        {
+    bool isNonDecreasing = true;//
+    for (int k = i; k <j; k++) 
+            {
+    if (v[k] > v[k + 1]) 
+                       {
+                        isNonDecreasing = false;
+                        break;
+                        }
+            }
+            if (isNonDecreasing) 
+            {
+                count++;
+            }
+        }
+}
+cout<<count<<endl;
+}        
+
 return 0;
 }
+ 
